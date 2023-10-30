8f9f5c6fbd5c816f7aa165e43f616c625a8ab1e2
AimePazzo <ndagijimanapazo64@gmail.com>
2023-10-30 18:29:45 +0200

    New Modified

--- index.js
+++ index.js
@@ -37,3 +37,26 @@ const shadowHeader = () =>{
                        : header.classList.remove('shadow-header')
 }
 window.addEventListener('scroll', shadowHeader)
+
+
+// Email JS
+
+const contactForm = document.getElementById("contact-form"),
+        contactMessage = document.getElementById('contact-message');
+
+        const sendEmail = (e) =>{
+            e.preventDefault();
+
+            emailjs.sendForm('service_kzutdjf','template_h8w87eq','#contact-form','mbdQfTjlWxjvdh2ij').then(() =>{
+                contactMessage.textContent = 'Message sent successfully ✅'
+                setTimeout(()=>{
+                    contactMessage.textContent =''
+                },5000)
+
+                contactForm.reset()
+            }, () =>{
+                contactMessage.textContent ='Message not sent (service error) ❌'
+            })
+        }
+
+        contactForm.addEventListener('submit', sendEmail);
